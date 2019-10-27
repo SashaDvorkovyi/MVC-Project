910fe83c70413eac062be0ca65e3a199d561a77b
WIN-H2UI3UGNKST\User <sasha3489@ukr.net>
2019-10-27 20:55:53 +0200

    no message

---
+++ Chat/Chat/ServiceChat.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+
+namespace Chat
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class ServiceChat : IServiceChat
+    {
+        List<ServerUser> users = new List<ServerUser>();
+
+        static int nextId = 1;
+
+        public int Connect(string name)
+        {
+            ServerUser user = new ServerUser()
+            {
+                Id = nextId,
+                Name = name,
+                operationContext = OperationContext.Current
+            };
+            nextId++;
+            SendMssage(user.Name + ": подключился к чату", nextId);
+            users.Add(user);
+
+            return user.Id;
+        }
+
+        public void Disconnect(int id)
+        {
+            var user = users.FirstOrDefault(x => x.Id == id);
+            if (user != null)
+            {
+                users.Remove(user);
+                SendMssage(user.Name + ": покинул чату", 0);
+            }
+        }
+
+        public void SendMssage(string message, int id)
+        {
+            if (users.Count > 0)
+            {
+                string answer = "(" + DateTime.Now.ToShortTimeString() + ") ";
+
+                var userSend = users.FirstOrDefault(x => x.Id == id);
+
+                if (userSend != null)
+                {
+                    answer += userSend.Name + ": ";
+                }
+
+                answer += message;
+
+                foreach (var user in users)
+                {
+                    user.operationContext.GetCallbackChannel<IServerChatCallback>().MessageCollback(answer);
+                }
+            }
+        }
+    }
+}
---
+++ Chat/ChatClient/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using ChatClient.ServiceChat;
+
+namespace ChatClient
+{
+    public partial class MainWindow : Window, IServiceChatCallback
+    {
+        bool isConnected = false;
+        ServiceChatClient client;
+        int id;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConnectUser()
+        {
+            if (!isConnected)
+            {
+                client = new ServiceChatClient(new System.ServiceModel.InstanceContext(this));
+                id = client.Connect(tbUserName.Text);
+                tbUserName.IsEnabled = false;
+                bConnDisconn.Content = "Disconect";
+                isConnected = true;
+            }
+        }
+
+        private void DisconnectUser()
+        {
+            if (isConnected)
+            {
+                client.Disconnect(id);
+                client = null;
+                tbUserName.IsEnabled = true;
+                bConnDisconn.Content = "Connected";
+                isConnected = false;
+            }
+        }
+
+        private void Batton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isConnected)
+            {
+                ConnectUser();
+            }
+            else
+            {
+                DisconnectUser();
+            }
+        }
+
+        public void MessageCollback(string message)
+        {
+            lbChat.Items.Add(message);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            DisconnectUser();
+        }
+
+        private void TbMessage_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key==Key.Enter)
+            {
+                if (client != null)
+                {
+                    client.SendMssage(tbMessage.Text, id);
+                    tbMessage.Text = string.Empty;
+                }
+            }
+        }
+    }
+}
